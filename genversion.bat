b702ae8efdd8b81aa3b19281afd4e0b1a009961e
Kormick <mrkormick@gmail.com>
2016-11-10 23:37:50 +0300

    Updated genversion script.

--- genversion.bat
+++ genversion.bat
@@ -4,8 +4,8 @@ setlocal enableextensions enabledelayedexpansion
 
 for /f %%v in ('git describe --tags --long') do set version=%%v
 
-set version_out=Version: "%version%"
-
-echo %version_out%> version.java
-
-pause
+echo package com.example.kormick.vlavashe_client^; > app\src\main\java\com\example\kormick\vlavashe_client\Version.java
+echo // Version: "%version%" >> app\src\main\java\com\example\kormick\vlavashe_client\Version.java
+echo public class Version { >> app\src\main\java\com\example\kormick\vlavashe_client\Version.java
+echo     public static final String version = "%version%"^; >> app\src\main\java\com\example\kormick\vlavashe_client\Version.java
+echo } >> app\src\main\java\com\example\kormick\vlavashe_client\Version.java
