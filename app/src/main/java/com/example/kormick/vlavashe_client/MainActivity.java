2951754a2dfd20cafc76a2d3e6bd467911aa9f11
Kormick <mrkormick@gmail.com>
2016-11-10 23:39:26 +0300

    Added text view with app version.

--- app/src/main/java/com/example/kormick/vlavashe_client/MainActivity.java
+++ app/src/main/java/com/example/kormick/vlavashe_client/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kormick.vlavashe_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView versionView = (TextView)findViewById(R.id.textView2);
+        versionView.setText(Version.version);
     }
 }
