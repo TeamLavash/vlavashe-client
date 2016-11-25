5df71b5c6031ad88aa7ab08bac65a14d3db8158f
Kormick <mrkormick@gmail.com>
2016-11-25 05:23:19 +0300

    Fixed genversion for linux.

--- genversion.sh
+++ genversion.sh
@@ -2,8 +2,8 @@ VERSION=$(git describe --tags --long)
 
 VERSION_FILE=app/src/main/java/com/example/kormick/vlavashe_client/Version.java
 
-echo "package com.example.kormick.vlavashe_client;"       >  $VERSION_FILE
-echo "// Version: "$VERSION_FILE""                        >> $VERSION_FILE
-echo "public class Version {"                             >> $VERSION_FILE
-echo "\tpublic static final String version = "$VERSION";" >> $VERSION_FILE
-echo "}"                                                  >> $VERSION_FILE
+echo "package com.example.kormick.vlavashe_client;"         >  $VERSION_FILE
+echo "// Version: $VERSION"                                 >> $VERSION_FILE
+echo "public class Version {"                               >> $VERSION_FILE
+echo "\tpublic static final String version = \"$VERSION\";" >> $VERSION_FILE
+echo "}"                                                    >> $VERSION_FILE
