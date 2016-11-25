0e0862f08c879e36df9fc30e4fb813b8a32028e6
Kormick <mrkormick@gmail.com>
2016-11-25 05:30:06 +0300

    Added Google Maps activity.

--- app/src/main/java/com/example/kormick/vlavashe_client/MainActivity.java
+++ app/src/main/java/com/example/kormick/vlavashe_client/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.kormick.vlavashe_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.kormick.vlavashe_client.DataBase.ShawarmaDataBase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView versionView = (TextView)findViewById(R.id.textView2);
+        TextView versionView = (TextView)findViewById(R.id.versionTextView);
         versionView.setText(Version.version);
+
+        dBase = new ShawarmaDataBase();
+
+        Button mapButton = (Button)findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("db", dBase);
+                startActivity(intent);
+            }
+        });
     }
+
+    private ShawarmaDataBase dBase;
 }
